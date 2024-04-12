e7a86229b021179f99e707a11d2087eebcda7de9
unknown <diego.castellanos-a@labinfo.is.escuelaing.edu.co>
2024-04-11 19:00:27 -0500

    Respuestas teorico

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,15 @@
-INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('1', 'Z', '123');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('1', 'b', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('2', 'b' '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('3', 'd', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('4', 'b', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('5', 'c', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('6', 'a', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('7', 'a', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('8', 'c', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('9', 'a', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('10', 'b', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('11', 'c', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('12', 'd', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('13', 'b', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('14', 'a', '.');
+INSERT INTO DIEGO_CASTELLANOS (Numero, Letra, Explicacion) VALUES ('15', 'b', '.');
